fc1a76ec3beefc2dab53777f8d77aa800c5accbe
Mohammad Wasil <88258099+MohWasil@users.noreply.github.com>
2026-01-11 13:21:12 +0430

    Preparing the codes for deployment

--- backend/rag_with_memory.py
+++ backend/rag_with_memory.py
@@ -160,7 +160,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 from pathlib import Path
 
-# Initalizing the .env path
 env_path = Path(__file__).resolve().parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
