0169745650df1343dab3f265f65b92f2610bd00a
Mohammad Wasil <88258099+MohWasil@users.noreply.github.com>
2026-01-17 21:26:07 +0430

    cleaning files

--- backend/entrypoint.py
+++ backend/entrypoint.py
@@ -7,9 +7,7 @@ role = os.getenv("ROLE", "gateway")
 if role == "worker":
     # IMPORT the logic for your Agent/MQTT subscriber
     from agent import SupportAgent 
-    SupportAgent() # This starts the loop that waits for questions
+    SupportAgent() 
 else:
-    # IMPORT the FastAPI app
     from api_mqtt import app 
-    # Start the web server
     uvicorn.run(app, host="0.0.0.0", port=8000)
